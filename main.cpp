74beaab050b5de05b568955ba1d5210daceea5c9
Gus Levinson <al2221@texel42.doc.ic.ac.uk>
2021-11-10 18:32:18 +0000

    Rotor class created with mapping function that works

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+
+enum letters{A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z};
+
+using namespace std;
+
+class rotor{
+  public:
+
+      // data members
+      int starting_pos; //that just needs to be a integer, as tells you which letter at 12 o'clock
+      int notches[26]; //make dynamic
+      int map[26][2];
+
+      // member function declarations
+      int mapping(int starting_pos, int map[26], int letter_dig); //does letter_dig also have to be a data memeber?
+      // i.e. can you only use data members of a class in the member function?
+};
+
+int main(){
+  cout << endl << endl;
+
+  rotor.starting_pos = 5;
+  int digit = 9;
+  int map[26];
+
+  cout << endl << endl;
+
+  rotor.mapping(starting_pos, map, digit);
+
+  return 0;
+}
+
+
+
+
+
+
+int mapping(int starting_pos, int map[26], int inputted_digit){
+  // the inputted digit is in absolute form (i.e. not taking into account rotation)
+  int rotation_transformation_1;
+  int rotation_transformation_2;
+  int mapped_digit;
+  cout << "Starting position (digit at 12 o'clock): " << starting_pos<<endl;
+
+  cout << "Inputted (absolute) digit is: " << inputted_digit<<endl;
+  // factoring in rotation to the inputted_digit
+  // transforming inputted digit (absolute form) to corresponding relative form
+  rotation_transformation_1 = inputted_digit + starting_pos; //takes into account rotation
+
+  if (rotation_transformation_1 >= 26){
+    rotation_transformation_1 = rotation_transformation_1 - 26;
+  }
+  cout << "Relative version is: " << rotation_transformation_1<<endl;
+
+  // mapping the relative digit
+  mapped_digit = map[rotation_transformation_1];
+  cout << "Which has been mapped to: " << mapped_digit << endl;
+
+  // removing affects of rotation
+  // transforming relative form to corresponding absolute form
+  rotation_transformation_2 = mapped_digit - starting_pos;
+
+  if (rotation_transformation_2 < 0){
+    rotation_transformation_2 = rotation_transformation_2 + 26;
+  }
+
+  cout << "Absolute version of this is: " << rotation_transformation_2 << endl << endl;
+  return rotation_transformation_2;
+}
